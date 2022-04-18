ff4c2aacc3a686b71456b8834d20313f65e50dc4
cleberjamaral <cleberjamaral@gmail.com>
2022-04-18 08:43:16 -0300

    Versão oficial

--- Gerar_Carteirinhas.py
+++ Gerar_Carteirinhas.py
@@ -5,8 +5,7 @@ import pandas as pd
 url_db = ('./consulta_geral_discente.csv')
 df = pd.read_csv(url_db, sep=';', encoding='iso-8859-1')
 
-def delete_files(extension):
-    directory = "./"
+def delete_files(directory,extension):
     files = os.listdir(directory)
     filtered = [file for file in files if file.endswith("." + extension)]
     for file in filtered:
@@ -15,17 +14,21 @@ def delete_files(extension):
 
 def generate_cards():
 
+    if not os.path.exists("./output"):
+        os.makedirs("./output")
+    os.chdir('./output')
     for student in df.index:
         card, hash = c.generate_card(df['Curso'][student],df['Nome'][student],df['Matrícula'][student])
 
         # Write test TEX and PDF files
-        output_file = "Carteirinha_" + df['Curso'][student] + "_" + df['Nome'][student] + ".tex"
+        output_file = "Carteirinha_" + df['Curso'][student].replace(" ", "_") + "_" + df['Nome'][student].replace(" ", "_") + ".tex"
         with open(output_file, 'w') as f:
             f.write("".join(card))
         os.system('xelatex -synctex=1 "' + output_file + '"')
 
 generate_cards()
-delete_files("log")
-delete_files("out")
-delete_files("gz")
-delete_files("aux")
+delete_files("./","log")
+delete_files("./","out")
+delete_files("./","gz")
+delete_files("./","aux")
+delete_files("./","tex")
